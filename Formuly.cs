47145a91fb7c27702be94fae008bd608ab5b15de
Lite <artem.artamonov.94@bk.ru>
2022-06-22 22:51:52 +0400

    Добавьте файлы проекта.

---
+++ Formuly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_Pim
+{
+    public class Formuly
+    {
+        public double Form1(double m)
+        {
+            double znam = (Math.Pow(3 * m + 2, 2) - 24m) ^ (-0, 5) / 3 * (m) ^ (-0, 5) - 2 / (m) ^ (-0, 5);
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1_Pim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для вычисления по формулам");
+            Console.WriteLine("z1 = ((3m + 2)^2 - 24m)^(-0,5) / 3 * (m)^(-0,5) - 2 / (m)^(-0,5)");
+            Console.WriteLine("z2 = -(m)^(-0,5)");
+            Console.WriteLine("Введите m");
+            double m = Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
